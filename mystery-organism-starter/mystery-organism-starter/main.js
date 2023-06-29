919d9487bd5f7ade43bf4f3a0dad3695f7006859
mdtycho <mdtycho@gmail.com>
2023-06-29 09:37:39 +0200

    completed mysterious organism project

--- mystery-organism-starter/mystery-organism-starter/main.js
+++ mystery-organism-starter/mystery-organism-starter/main.js
@@ -13,10 +13,55 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+function pAequorFactory(organismNum, dna){
+  return {
+    specimenNum:organismNum,
+    dna:dna,
+    mutate:function(){
+      let randomIndex = Math.floor(Math.random() * 15);
+      let randomBase = returnRandBase();
+      while(this.dna[randomIndex] === randomBase){
+        randomBase = returnRandBase();
+      }
+
+      this.dna[randomIndex] = randomBase;
+
+      return this.dna;
+    },
+    compareDNA:function(other){
+      let count = 0;
+      for (let index = 0; index < other.dna.length; index++) {
+        if(other.dna[index] === this.dna[index]){
+          count++;
+        }
+        
+      }
+
+      console.log(`specimen #${this.specimenNum} and specimen #${other.specimenNum} have ${(count/15)*100}% in common`);
+    },
+    willLikelySurvive:function(){
+      let count = 0;
+      for (const iterator of this.dna) {
+        if (iterator === 'C' || iterator === 'G') {
+          count++;
+        }
+      }
+
+      if ((count/this.dna.length)>=0.6) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  };
+}
+
+let paequorInstances = [];
+
+for (let index = 0; index < 30; index++) {
+  paequorInstances.push(new pAequorFactory(index+1, mockUpStrand()));
+  
+}
 
 
-
-
-
-
-
+console.log(paequorInstances);
